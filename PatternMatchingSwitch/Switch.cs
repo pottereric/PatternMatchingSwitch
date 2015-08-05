b9712c3932a9d9b88cb1f57f006950cf504899c4
pottereric <eric.b.potter@gmail.com>
2015-08-05 09:25:18 -0400

    Working for a tuple of bool, bool

---
+++ PatternMatchingSwitch/Switch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatchingSwitch
+{
+    public class Switch<T, T1, T2> where T : Tuple<T1, T2>
+    {
+        private T _switchable;
+        private bool found = false;
+
+        public Switch(T item)
+        {
+            _switchable = item;
+        }
+
+        public Switch<T, T1, T2> Case(T compareable, Action method )
+        {
+            if(compareable.Item1.Equals(_switchable.Item1) && compareable.Item2.Equals(_switchable.Item2) && !found)
+            {
+                method();
+                found = true;
+            }
+
+            return this;
+        }
+
+        public Switch<T, T1, T2> Case(T1 compareableA, T2 comparableB, Action method)
+        {
+            if (compareableA.Equals(_switchable.Item1) && comparableB.Equals(_switchable.Item2) && !found)
+            {
+                method();
+                found = true;
+            }
+
+            return this;
+        }
+
+        public void Default(Action method)
+        {
+            if (!found)
+            {
+                method();
+            }
+        }
+
+    }
+}
---
+++ PatternMatchingSwitch_UnitTests/WhenSwitchingOnATupleOfBool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternMatchingSwitch;
+
+namespace PatternMatchingSwitch_UnitTests
+{
+    [TestClass]
+    public class WhenSwitchingOnATupleOfBool
+    {
+        private Tuple<bool, bool> IsMultipleOfThreeMultipleOfFive(int i)
+        {
+            return new Tuple<bool, bool>(i % 3 == 0, i % 5 == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            foreach (var i in Enumerable.Range(1, 20))
+            {
+                
+                var value = IsMultipleOfThreeMultipleOfFive(i);
+
+                new Switch<Tuple<bool, bool>, bool, bool>(value)
+                    .Case(true, true, 
+                        () => Debug.WriteLine("FizzBuzz"))
+                    .Case(true, false, 
+                        () => Debug.WriteLine("Fizz"))
+                    .Case(false, true,  
+                        () => Debug.WriteLine("Buzz"))
+                    .Default(
+                        () => Debug.WriteLine(i))
+                    ;
+            }
+        }
+    }
+}
